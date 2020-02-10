2db6a7e043fc21a1f63f78fec55f08e1b852b8c6
jkoeninger <jkoeninger@nexigen.com>
2020-02-10 14:51:52 -0500

    socket, express, and react setup

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import io from 'socket.io-client'
+
+
+import './App.css';
+
+export default class App extends React.Component {
+  
+  socket = io(`http://localhost:3001`);
+  
+
+  componentDidMount = _ => {
+    this.socket.on('connection', (socket) => {
+      //socket.broadcast.emit('hi');
+      //socket.broadcast.emit('test');
+      //socket.emit('test');
+    });
+  }
+
+  btnClick = () => {
+    this.socket.emit('test')
+  }
+
+  render(){
+    return (
+      <div className="App">
+        ...
+        <br />
+        <button onClick={this.btnClick}>Click</button>
+      </div>
+    );
+  }
+
+}
