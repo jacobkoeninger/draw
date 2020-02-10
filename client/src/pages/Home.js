ea72ba77eab99c385f1414a72bca1210019241f3
jkoeninger <jkoeninger@nexigen.com>
2020-02-10 16:39:57 -0500

    getting ready to start working on rooms and nicknames

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,6 +2,19 @@ import React from 'react';
 
 export default class Home extends React.Component {
 
+    /* 
+       Title
+       
+       block inputs:
+        - Room id,
+        - (maybe room password),
+        - nickname
+        - join button
+
+        Create room button (require nickname as well)
+
+    */
+
     render(){
         return (
             <div>
