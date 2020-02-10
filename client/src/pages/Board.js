32044a71eb0a17d712975bb541fddf21625111b0
jkoeninger <jkoeninger@nexigen.com>
2020-02-10 15:00:25 -0500

    setup pages and router

---
+++ client/src/pages/Board.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Board extends React.Component {
+
+    render(){
+        return (
+            <div>
+                Board
+            </div>
+        );
+    }
+}
